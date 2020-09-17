cce1c83f67b352719ebbe69972ce8290fff6bf26
Jose Ángel Gumiel <jagumiel001@gmail.com>
2020-09-17 23:36:45 +0200

    Code Improved
    -writeChar(uint16_t) method created.
    
    All the characters are showed in the display.
    Characters change every 500ms.

--- TrainingProjects/SPI-LEDs_Driver_Test/test-driverLed.cydsn/main.c
+++ TrainingProjects/SPI-LEDs_Driver_Test/test-driverLed.cydsn/main.c
@@ -10,67 +10,28 @@
  * ========================================
 */
 #include "project.h"
+#include "alphabet.h"
 
-int main(void)
-{
-    CyGlobalIntEnable; /* Enable global interrupts. */
+void writeChar(uint16_t myChar){
+    SPI_1_SpiUartWriteTxData(myChar);
+    LE_Write(1);
+    LE_Write(0);
+}
 
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+int main(void){
+    /* Enable global interrupts. */
+    CyGlobalIntEnable;
+
+    /* Component initialization/startup */
     Clock_1_Start();
     SPI_1_Start();
-    //uint16_t dato=0;
     
 
-    for(;;)
-    {
-        CyDelay(500);
-        //dato=0x003F;
-        
-        /*SPI_1_SpiUartWriteTxData(0);
-        CyDelay(1);
-        SPI_1_SpiUartWriteTxData(0);
-        CyDelay(1);
-        SPI_1_SpiUartWriteTxData(1);
-        CyDelay(1);
-        SPI_1_SpiUartWriteTxData(1);
-        CyDelay(1);
-        SPI_1_SpiUartWriteTxData(1);
-        CyDelay(1);
-        SPI_1_SpiUartWriteTxData(1);
-        CyDelay(1);
-        SPI_1_SpiUartWriteTxData(1);
-        CyDelay(1);
-        SPI_1_SpiUartWriteTxData(1);
-        CyDelay(1);*/
-        SPI_1_SpiUartWriteTxData(0b0000000000111111); //'0' literal segun el otro codigo
-        //SPI_1_SpiUartWriteTxData(0b1111110000000000); //lo volteo
-        //SPI_1_SpiUartWriteTxData(0xFC00); // la vuelta, la paso a hex.
-        //0b0000001100110111='A' -> 0b1110110011000000 -> ECC0
-        //SPI_1_SpiUartWriteTxData(0xECC0);
-        //SPI_1_SpiUartWriteTxData(0b0000001100110111);
-        /*if(dato<65535)
-            dato++;
-        else
-            dato=0;
-        CyDelay(1);*/
-        
-        
-        LE_Write(1);
-        LE_Write(0);
-        
-        
-        CyDelay(500);
-        SPI_1_SpiUartWriteTxData(0b0000001100110111); //A
-        /*if(dato<65535)
-            dato++;
-        else
-            dato=0;
-        CyDelay(1);*/
-        
-        
-        LE_Write(1);
-        LE_Write(0);
-        
+    for(;;){
+        for(int i=0; i<10; i++){
+            CyDelay(500);
+            writeChar(ALPHANUMR_CHAR_TABLE[i]);
+        }
     }
 }
 
